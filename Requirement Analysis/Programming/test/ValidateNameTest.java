6f6820f649325feb276066bdbd2b6c82369e0921
Hoang.lh194766 <91466211+LeeHuyyHoangg@users.noreply.github.com>
2021-12-06 04:46:53 -0800

    Add files via upload

---
+++ Requirement Analysis/Programming/test/ValidateNameTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import controller.PlaceOrderController;
+
+public class ValidateNameTest {
+
+	private PlaceOrderController placeOrderController;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		placeOrderController = new PlaceOrderController();
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"NguyenThiA,true",
+		"@NguyenThiA,false",
+		"NguyenTh1A,false",
+		"Nguyen Thi A,true",
+		"NGUYENTHIA,true",
+		"@NGUYENTHIA,false",
+		"NGUYENTH1A,false",
+		"NGUYEN THI A,true",
+		"Dr.James,true",
+		"          ,false",
+		",false"
+	})
+
+	public void test(String name, boolean expected) {
+		assertEquals(expected, placeOrderController.validateName(name));
+	}
+
+}
